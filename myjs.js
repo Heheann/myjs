a7f24fe61bdb1a13209f3435ef83294295aab91f
he <77sevenlucky77@gmail.com>
2026-06-15 20:36:16 +0800

    1st

---
+++ myjs.js
@@ -0,0 +1,7 @@
+
+        function changeHead(){
+            document.getElementById("myh1").style.color="pink";
+        }
+        function clearHead(){
+            document.getElementById("myh1").style.color="inherit";
+        }
